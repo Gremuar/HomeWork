364abec2508a39b39b02bfb4a03625ba1daeda30
Xenon <gg.riptor@gmail.com>
2020-10-11 22:55:56 +0700

    Задание 3-1

---
+++ src/ru/home_work/work_3_1/Palindrome.java
@@ -0,0 +1,21 @@
+package ru.home_work.work_3_1;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println(IsPalindrome("ТопоТ"));
+    }
+    public static boolean IsPalindrome(String word) {
+        word = word.toLowerCase();
+        int wLen = word.length();
+        if(wLen < 3 || wLen % 2 == 0)
+            return false;
+        else if(wLen == 3) {
+            return word.charAt(0) == word.charAt(wLen - 1);
+        }
+        else if(word.charAt(0) == word.charAt(wLen - 1)) {
+            return IsPalindrome(word.substring(1, wLen - 1));
+        }
+        else
+            return false;
+    }
+}
