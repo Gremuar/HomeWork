f5c4e8cec16bb7648d694a2aa7868de29860671f
Xenon <gg.riptor@gmail.com>
2020-10-13 22:34:26 +0700

    Добавлено задание 3-3 Реверс массива

--- src/ru/home_work/work_3/ArrayListFilter.java
+++ src/ru/home_work/work_3/ArrayListFilter.java
@@ -6,17 +6,17 @@ import java.util.Collections;
 
 //#3.2
 public class ArrayListFilter {
+//        Написать функцию, которая убирает одинаковые значения из списка.
+//        1 1 1 1 2 2 3 4 5
+//        Результат программы:
+//        1 2 3 4 5
+//        *******************
     public static void main(String[] args) {
         var arr = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 7, 9, 1, 15, 2, 3, 5, 5, 6, 10, 5, 15));
         System.out.println(arrayListFilter(arr));
     }
 
     public static ArrayList<Integer> arrayListFilter(ArrayList<Integer> list) {
-//        Написать функцию, которая убирает одинаковые значения из списка.
-//        1 1 1 1 2 2 3 4 5
-//        Результат программы:
-//        1 2 3 4 5
-//        *******************
         var newList = new ArrayList<Integer>();
         newList.add(list.get(0));
         Collections.sort(list);
