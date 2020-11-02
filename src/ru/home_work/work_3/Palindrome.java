a9a763f9c67d5824ab8a03f9bdb9e399705ba9be
Xenon <gg.riptor@gmail.com>
2020-11-02 16:30:19 +0700

    ДЗ №3-1 Палиндром: * Добавлена проверка на null" * Теперь корректно обрабатываются 2х символьные палиндромы * Исправлено название метода в стиле lowerCamelCase

--- src/ru/home_work/work_3/Palindrome.java
+++ src/ru/home_work/work_3/Palindrome.java
@@ -11,13 +11,18 @@ public class Palindrome {
 //    **************
     public static void main(String[] args) {
         System.out.println(isPalindrome("ТопоТ"));
+        System.out.println(isPalindrome("Яя"));
+        System.out.println(isPalindrome(null));
     }
 
     public static boolean isPalindrome(String word) {
-        word = word.toLowerCase();
+        word = word != null ? word.toLowerCase() : "";
         int wLen = word.length();
-        if (wLen < 3 || wLen % 2 == 0)
+        if (wLen == 2)
+            return word.charAt(0) == word.charAt(1);
+        else if (wLen < 2 || wLen % 2 == 0) {
             return false;
+        }
         else if (wLen == 3) {
             return word.charAt(0) == word.charAt(wLen - 1);
         } else if (word.charAt(0) == word.charAt(wLen - 1)) {
